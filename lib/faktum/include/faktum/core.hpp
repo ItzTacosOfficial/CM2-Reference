168a615c806033aef135c8b922c6208cd02f7c07
ItzTacos <47322543+ItzTacosOfficial@users.noreply.github.com>
2023-10-25 17:33:00 +0200

    Enabled FAK_IMPORT

--- lib/faktum/include/faktum/core.hpp
+++ lib/faktum/include/faktum/core.hpp
@@ -5,7 +5,7 @@
 #include <Windows.h>
 
 
-#define FAK_IMPORT /*__declspec(dllimport)*/
+#define FAK_IMPORT __declspec(dllimport)
 
 #define FAK_SIZE_GUARD(class, value) static_assert(sizeof(class) == (value), "Size of class '"#class"' does not match expected value '"#value"'")
 

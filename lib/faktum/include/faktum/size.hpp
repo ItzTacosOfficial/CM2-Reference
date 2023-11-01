dfc3819a35b37097fa7b09c86e36328348223dd4
ItzTacos <47322543+ItzTacosOfficial@users.noreply.github.com>
2023-11-01 23:52:42 +0100

    Added FSize

---
+++ lib/faktum/include/faktum/size.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "core.hpp"
+
+
+class FVector2;
+
+class FAK_IMPORT FSize {
+
+public:
+
+	FSize(float width, float height);
+	FSize();
+
+	FSize& operator=(const FSize& other);
+	BOOL operator==(const FSize& other) const;
+	BOOL operator!=(const FSize& other) const;
+	operator FVector2() const;
+	FSize operator-(const FSize& other) const;
+	FSize operator+(const FSize& other) const;
+	FSize operator+=(const FSize& other);
+	FSize operator-=(const FSize& other);
+
+	float Area() const;
+	BOOL IsZero() const;
+
+
+	float width;
+	float height;
+
+};
+FAK_SIZE_GUARD(FSize, 0x8);
